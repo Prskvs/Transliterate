6daa72e4d6bd4d27981df54ad2f76b7747591383
Prskvs <29263845+Prskvs@users.noreply.github.com>
2019-12-02 11:04:55 +0200

    Revised

--- Transliterate.php
+++ Transliterate.php
@@ -43,55 +43,5 @@ class Transliterate {
         return mb_strtolower($new);
     }
 
-    /**
-     * Depricated
-     * @param $str
-     * @return string
-     */
-    public function toSlug_depricated($str) {
-        $splitted = preg_split("//u", $str, -1, PREG_SPLIT_NO_EMPTY);
-        $new = "";
-        $g = FALSE;
-        $max = count($splitted) - 1;
-        foreach ($splitted as $key => $char):
-            if ($char == " "):
-                if ($key < $max && $splitted[$key+1] == "-"):
-                    continue;
-                else:
-                    $new .= "-";
-                    continue;
-                endif;
-            endif;
-
-            if ($key < $max):
-                if ($char == "-" && $splitted[$key+1] == " ")
-                    continue;
-            endif;
-
-            if (preg_match("/[0-9a-zA-Z`~!@#$%^&*()_+-=--==;':,.\<\>\|\[\]\{\}\?\"]/", $char)){
-                $new .= $char;
-                continue;
-            }
-
-            foreach($this->_greek as $gkey => $gr):
-                if ($key > 0 && ($char == "υ" || $char == "ύ") && in_array($splitted[$key-1], array("ο", "ε", "α"))):
-                    $new .= "u";
-                    $g = TRUE;
-                    break;
-
-                elseif ($char == $gkey):
-                    $new .= $gr;
-                    $g = TRUE;
-                    break;
-                endif;
-            endforeach;
-
-            if ($g == FALSE):
-                $new .= $char;
-            endif;
-        endforeach;
-
-        return mb_strtolower($new);
-    }
 }
 ?>
